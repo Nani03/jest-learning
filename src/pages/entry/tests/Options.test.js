ffc157bd1e6371163e867b058270191ec92b74e9
Nani03 <leela.rroyal@gmail.com>
2023-09-28 15:43:07 +0530

    code and test scoop option image

---
+++ src/pages/entry/tests/Options.test.js
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import Option from "../Option";
+
+test("should displays image for each scoop", async () => {
+  render(<Option optionType="scoops" />);
+  // find images
+  const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
+  expect(scoopImages).toHaveLength(2);
+
+  //confirm alt text of images
+  const alttext = scoopImages.map((el) => el.alt);
+  expect(alttext).toEqual(["chocolate scoop", "vanilla scoop"]);
+});
